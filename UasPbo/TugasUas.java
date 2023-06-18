1f090b2c81d97b36d217b82488e626bc407a3d3f
FitraNoviarji <113648587+FitraNoviarji@users.noreply.github.com>
2023-06-19 01:00:19 +0800

    Add files via upload

---
+++ UasPbo/TugasUas.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugasuas;
+
+/**
+ *
+ * @author asus
+ */
+public class TugasUas {
+
+    public static void main(String[] args) {
+        Login Login = new Login();
+        Login.setVisible(true);
+
+    }
+}
