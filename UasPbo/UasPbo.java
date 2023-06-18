81220e27644ae1e308b5703b869d67407e7596a9
FitraNoviarji <113648587+FitraNoviarji@users.noreply.github.com>
2023-06-19 02:59:21 +0800

    Add files via upload

---
+++ UasPbo/UasPbo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */ 
+package javaapplication2;
+
+/**
+ *
+ * @author asus
+ */
+public class UasPbo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TampilkanData tampilkanData = new TampilkanData();
+        tampilkanData.setVisible(true);
+           
+    }
+    
+}
